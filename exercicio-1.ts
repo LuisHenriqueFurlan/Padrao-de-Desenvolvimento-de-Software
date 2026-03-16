40bb0e708071c1cc174f121d863787dfdd3d7302
Aluno TI <alunoti@univaglabs.iemat>
2026-03-16 10:47:37 -0400

    Adicionando os outros exercicios referentes a codigo prático

--- exercicio-1.ts
+++ exercicio-1.ts
@@ -1,41 +1,67 @@
 interface Funcionario {
  trabalhar(): void;
  registrarPonto(): void;
- receberSalario(): void;
- gerenciarEquipe(): void;
- escreverCodigo(): void;
 }
 
-class Gerente implements Funcionario {
+interface Gerente extends Funcionario {
+gerenciarEquipe(): void
+}
+
+interface Desenvolvedor extends Funcionario {
+receberSalario(): void
+escreverCodigo(): void
+}
+
+interface Estagiário extends Funcionario {
+escreverCodigo(): void
+}
+
+interface Freelancer {
+trabalhar(): void
+escreverCodigo(): void
+}
+
+interface ISistemaRH {
+nome: string,
+CPF: string,
+identificacao: number
+}
+
+class SistemaRH implements ISistemaRH {
+nome: string
+CPF: string
+identificacao: number;
+
+constructor(nome: string, CPF: string, identificacao: number) {
+this.nome = nome,
+this.CPF = CPF,
+this.identificacao = identificacao
+}
+}
+
+class Gerente implements Gerente {
  trabalhar(): void { console.log("Gerente trabalhando"); }
  registrarPonto(): void { console.log("Ponto registrado"); }
  receberSalario(): void { console.log("Salário recebido"); }
  gerenciarEquipe(): void { console.log("Gerenciando equipe"); }
- escreverCodigo(): void {
- throw new Error("Gerente não escreve código");
- }
 }
 
-class Desenvolvedor implements Funcionario {
+class Desenvolvedor implements Desenvolvedor {
  trabalhar(): void { console.log("Desenvolvedor trabalhando"); }
  registrarPonto(): void { console.log("Ponto registrado"); }
  receberSalario(): void { console.log("Salário recebido"); }
- gerenciarEquipe(): void {
- throw new Error("Dev não gerencia equipe");
- }
  escreverCodigo(): void { console.log("Escrevendo código"); }
 }
 
-class Estagiario implements Funcionario {
+class Estagiario implements Estagiario {
  trabalhar(): void { console.log("Estagiário trabalhando"); }
  registrarPonto(): void { console.log("Ponto registrado"); }
- receberSalario(): void {
- throw new Error("Estagiário recebe bolsa, não salário");
- }
- gerenciarEquipe(): void {
- throw new Error("Estagiário não gerencia");
- }
  escreverCodigo(): void { console.log("Estagiário escrevendo código"); }
+}
+
+class Freelancer implements Freelancer {
+trabalhar(): void { console.log("Freelancer trabalhando"); }
+escreverCodigo(): void { console.log("Freelancer escrevendo código"); }
 }
 
  
